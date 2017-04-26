8c744d5b38aedd4e8999c9ae54e8acf8f2d1d229
Cleopatravii <fay-ith@hotmail.com>
2017-04-26 23:40:09 +0200

    Update and rename .gitignore to #1.py

---
+++ #1.py
@@ -0,0 +1,8 @@
+from string import maketrans
+intab = "abcdefghijklmnopqrstuvwxyz"
+outtab = "cdefghijklmnopqrstuvwxyzab"
+trantab = maketrans(intab,outtab)
+text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+text_1 ="map"
+print text.translate(trantab)
+print "url", text_1.translate(trantab)
